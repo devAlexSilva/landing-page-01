96da26547d42e7fd7cf98c983ec7d9229a29cebb
Alex Silva <9apsilva@gmail.com>
2022-09-16 13:13:31 -0300

    refactor: change router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Navbar } from "./components/navbar/Navbar";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/home/Home";
 
 export default function App() {
   return (
     <Router>
-        <Navbar />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </Router>
   );
 }
